56737819bd996b9d340992280b29c1024d8d3e35
Peter Ting <pting1995@gmail.com>
2020-12-16 18:23:33 -0800

    added array lists and confirmation interfaces for lower, upper, numeric, and special characters

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ function writePassword() {
   var specialChar = false;
 
   var lowerArr = [
-    
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
   ]
   var upperArr = [
-    
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
   ]
   var numArr = [
-    
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
   ]
   var specArr = [
-    
+    "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\\", "/", ":", ";", "\"", "'", "<", ">", ",", ".", "?",
   ]
   // all user selected characters are added to this main array to choose from
   var controlArr = [
@@ -36,7 +36,28 @@ function writePassword() {
   
   if (passLength >= 8 && passLength <= 128) {
     // ask user if they want lowercase, uppercase, numeric and/or special characters
-    alert("You picked a good number!")
+    lowerCase = confirm("Do you want lower case characters? There are " + lowerArr.length + " lower case characters.")
+    if (lowerCase === true) {
+      alert("lowercase was added")
+    }
+
+    upperCase = confirm("Do you want upper case characters? There are " + upperArr.length + " upper case characters.")
+    if (upperCase === true) {
+      // add uppercase to controlarr
+      alert("uppercase was added")
+    }
+    
+    numbericPass = confirm("Do you want numberic characters?There are " + numArr.length + " numeric characters.")
+    if (numbericPass === true) {
+      // add numbers to controlarr
+      alert("numbers was added")
+    }
+
+    specialChar = confirm("Do you want special characters? There are " + specArr.length + " special characters")
+    if (specialChar === true) {
+      // add special characters to controllarr
+      alert("special characters was added")
+    }
   }
     
   else {
